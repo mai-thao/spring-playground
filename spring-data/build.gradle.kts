069e3cf96db90880063f7fd79b8790e5e69c91c1
mai-thao <mthaominn@gmail.com>
2025-05-14 20:46:17 -0500

    Force Kotlin to support reflections

--- spring-data/build.gradle.kts
+++ spring-data/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.jetbrains.kotlin:kotlin-reflect") // Kotlin doesn't support reflection by default so explicitly force it to
     implementation("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
