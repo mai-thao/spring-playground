c61c64ced0f2fa0545f965c9d1d4923da5e0658d
mai-thao <mthaominn@gmail.com>
2025-05-16 23:22:53 -0500

    Create POST endpoint

--- spring-data/src/main/kotlin/com/example/userapp/controller/UserContoller.kt
+++ spring-data/src/main/kotlin/com/example/userapp/controller/UserContoller.kt
@@ -2,13 +2,14 @@ package com.example.userapp.controller
 
 import com.example.userapp.model.User
 import com.example.userapp.repository.UserRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/users")
 class UserContoller(private val userRepo: UserRepository) {
     @GetMapping
     fun getAll(): List<User> = userRepo.findAll()
+
+    @PostMapping
+    fun createUser(@RequestBody user: User): User = userRepo.save(user)
 }
