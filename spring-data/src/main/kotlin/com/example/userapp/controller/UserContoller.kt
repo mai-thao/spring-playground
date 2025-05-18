1b742b34bac091718d3b30b3216ac515aa0fd899
mai-thao <mthaominn@gmail.com>
2025-05-18 18:36:23 -0500

    Add PUT update logic

--- spring-data/src/main/kotlin/com/example/userapp/controller/UserContoller.kt
+++ spring-data/src/main/kotlin/com/example/userapp/controller/UserContoller.kt
@@ -2,11 +2,12 @@ package com.example.userapp.controller
 
 import com.example.userapp.model.UserData
 import com.example.userapp.repository.UserRepository
+import com.example.userapp.service.UserService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/users")
-class UserContoller(private val userRepo: UserRepository) {
+class UserContoller(private val userRepo: UserRepository, private val userService: UserService) {
     @GetMapping
     fun getAll(): List<UserData> = userRepo.findAll()
 
@@ -15,4 +16,9 @@ class UserContoller(private val userRepo: UserRepository) {
 
     @DeleteMapping
     fun deleteUser(@RequestParam id: Long) = userRepo.deleteById(id)
+
+    @PutMapping("/{id}")
+    fun updateUser(@PathVariable id: Long, @RequestBody userData: UserData): UserData {
+        return userService.updateUserData(id, userData)
+    }
 }
