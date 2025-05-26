7a51b27434e6cd8ffce76905ae3935c1d0d568e0
mai-thao <mthaominn@gmail.com>
2025-05-25 19:03:37 -0500

    Rename test class

--- spring-data/src/test/kotlin/com/example/userapp/UserControllerClass.kt
+++
@@ -1,4 +0,0 @@
-package com.example.userapp
-
-class UserControllerClass {
-}
---
+++ spring-data/src/test/kotlin/com/example/userapp/UserControllerClassTest.kt
@@ -0,0 +1,56 @@
+package com.example.userapp
+
+import com.example.userapp.model.UserData
+import com.example.userapp.service.UserService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+/**
+ * Uses the Mockk Mock testing framework along with the SpringMockk
+ */
+@WebMvcTest
+class UserControllerClass(@Autowired val mockMvc: MockMvc) {
+    @MockkBean
+    private lateinit var userService: UserService
+
+    @Test
+    fun `should get all users`() {
+        // Arrange
+        val mockUsers = listOf(
+            UserData(
+                id = 1,
+                name = "Jane",
+                gender = "Female",
+                email = "janesemail@company.com"
+            )
+        )
+
+        // Act
+        every {userService.getAllUsers()} returns mockUsers
+
+        // Assert
+        mockMvc.perform(get("/users"))
+            .andExpect(status().isOk)
+            .andExpect(content().json(
+                """
+                    [
+                    	{
+                    		"id": 1,
+                    		"name": "Jane",
+                    		"gender": "Female",
+                    		"email": "janesemail@company.com"
+                    	}
+                    ]
+                """.trimIndent()
+            ))
+
+        verify { userService.getAllUsers() }
+    }
+}
