e31eff36edc86018e111aeff20edb43053a529e2
mai-thao <mthaominn@gmail.com>
2025-05-26 10:04:27 -0500

    Add CREATE and UPDATE unit tests

--- spring-data/build.gradle.kts
+++ spring-data/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation("org.postgresql:postgresql")
 
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.19.0")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
--- spring-data/src/test/kotlin/com/example/userapp/UserControllerClassTest.kt
+++ spring-data/src/test/kotlin/com/example/userapp/UserControllerClassTest.kt
@@ -2,14 +2,16 @@ package com.example.userapp
 
 import com.example.userapp.model.UserData
 import com.example.userapp.service.UserService
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import io.mockk.verify
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest
@@ -22,7 +24,7 @@ class UserControllerClassTest(@Autowired val mockMvc: MockMvc) {
         // Arrange
         val mockUsers = listOf(
             UserData(
-                id = 1,
+                id = 1L,
                 name = "Jane",
                 gender = "Female",
                 email = "janesemail@company.com"
@@ -50,5 +52,78 @@ class UserControllerClassTest(@Autowired val mockMvc: MockMvc) {
         }
 
         verify { userService.getAllUsers() } // Verify getAllUsers() fxn is called once
+    }
+
+    @Test
+    fun `should create new user`() {
+        // Arrange
+        val inputUser = UserData(
+            id = 1L,
+            name = "Jane",
+            gender = "Female",
+            email = "janesemail@company.com"
+        )
+        val outputUser = inputUser.copy(id = 1L)
+
+        // Act
+        every { userService.createNewUser(inputUser) } returns outputUser
+
+        // Assert
+        mockMvc.post("/users") {
+            contentType = MediaType.APPLICATION_JSON
+            content = jacksonObjectMapper().writeValueAsString(inputUser)
+        } .andExpect {
+            status().isOk()
+            content().json(
+                """
+                    {
+                        "id": 1,
+                        "name": "Jane",
+                        "gender": "Female",
+                        "email": "janesemail@company.com"
+                    }
+                """.trimIndent()
+            )
+        }
+
+        verify { userService.createNewUser(inputUser) }
+    }
+
+    @Test
+    fun `should update existing user`() {
+        // Arrange
+        val userId = 1L
+        val userToUpdate = UserData(
+            email = "janesupdatedemail@company.com"
+        )
+        val updatedUser = UserData(
+            id = userId,
+            name = "Jane",
+            gender = "Female",
+            email = "janesupdatedemail@company.com"
+        )
+
+        // Act
+        every { userService.updateUserData(userId, userToUpdate) } returns updatedUser
+
+        // Assert
+        mockMvc.put("/users/$userId") {
+            contentType = MediaType.APPLICATION_JSON
+            content = jacksonObjectMapper().writeValueAsString(userToUpdate)
+        } .andExpect {
+            status().isOk()
+            content().json(
+                """
+                    {
+                        "id": 1,
+                        "name": "Jane",
+                        "gender": "Female",
+                        "email": "janesupdatedemail@company.com"
+                    }
+                """.trimIndent()
+            )
+        }
+
+        verify { userService.updateUserData(userId, userToUpdate) }
     }
 }
