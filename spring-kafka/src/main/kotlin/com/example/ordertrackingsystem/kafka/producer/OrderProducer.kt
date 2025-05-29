d388bd8e0b87f5b3c26bd9e9b86eec0eb2cf0334
mai-thao <mthaominn@gmail.com>
2025-05-28 22:12:09 -0500

    Producer has no business logic so change to a Component class

--- spring-kafka/src/main/kotlin/com/example/ordertrackingsystem/kafka/producer/OrderProducer.kt
+++ spring-kafka/src/main/kotlin/com/example/ordertrackingsystem/kafka/producer/OrderProducer.kt
@@ -3,14 +3,16 @@ package com.example.ordertrackingsystem.kafka.producer
 import com.example.ordertrackingsystem.model.Order
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 
 /**
- * See the Spring Kafka docs on using Kafka Template to send messages: https://docs.spring.io/spring-kafka/reference/kafka/sending-messages.html
- *
+ * @Component tells Spring to treat this class as a bean so it can scan and initialize it
  * @Value is used to inject externalized properties from application.yml config file
+ *
+ * This producer handles simple infrastructure-level message dispatching and does not contain business logic so it's not a @Service
+ * See the Spring Kafka docs on using Kafka Template to send messages: https://docs.spring.io/spring-kafka/reference/kafka/sending-messages.html
  */
-@Service
+@Component
 class OrderProducer(
     private val kafkaTemplate: KafkaTemplate<String, Order>,
     @Value("\${spring.kafka.template.default-topic}") private val topicName: String
