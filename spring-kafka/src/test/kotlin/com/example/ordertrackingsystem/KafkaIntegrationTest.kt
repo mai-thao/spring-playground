7430fd02be54cac89775d55c91a21742700152fc
mai-thao <mthaominn@gmail.com>
2025-05-31 09:40:47 -0500

    Add explaination comments to integration test

--- spring-kafka/src/test/kotlin/com/example/ordertrackingsystem/KafkaIntegrationTest.kt
+++ spring-kafka/src/test/kotlin/com/example/ordertrackingsystem/KafkaIntegrationTest.kt
@@ -15,6 +15,10 @@ import org.springframework.kafka.test.utils.KafkaTestUtils
 
 /**
  * Following Spring's recommendation for testing Kafka integration: https://docs.spring.io/spring-kafka/reference/testing.html
+ *
+ * @SpringBootTest boots up the full Spring application context for end-to-end integration testing
+ * @EmbeddedKafka starts an in-memory Kafka broker for integration testing (does not rely on Docker or external brokers)
+ * @Autowired injects Spring-managed beans into the test context
  */
 @SpringBootTest
 @EmbeddedKafka(partitions = 1, topics = ["order-tracking-topic"])
@@ -30,6 +34,7 @@ class KafkaIntegrationTest(
         // Arrange
         val order = Order(id = "123", productName = "Monopoly Boardgame", totalAmount = 19.99, totalQuantity = 1)
 
+        // Configurations for the test consumer to read from the embedded Kafka
         val consumerProps = KafkaTestUtils.consumerProps("test-group-id", "true", embeddedKafkaBroker)
         consumerProps["key.deserializer"] = StringDeserializer::class.java
         consumerProps["value.deserializer"] = JsonDeserializer::class.java
@@ -43,6 +48,6 @@ class KafkaIntegrationTest(
 
         // Assert
         val record = KafkaTestUtils.getSingleRecord(consumer, topic)
-        assertThat(record.value()).isEqualTo(order)
+        assertThat(record.value()).isEqualTo(order) // Assert correct deserialization
     }
 }
