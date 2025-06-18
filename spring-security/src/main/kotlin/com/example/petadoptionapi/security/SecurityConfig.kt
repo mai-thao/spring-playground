6b9e4a56dec0fc34faad4fe574da19f4f0af62a3
mai-thao <mthaominn@gmail.com>
2025-06-17 23:53:18 -0500

    Need to explicitly import invoke function

--- spring-security/src/main/kotlin/com/example/petadoptionapi/security/SecurityConfig.kt
+++ spring-security/src/main/kotlin/com/example/petadoptionapi/security/SecurityConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.config.annotation.web.invoke
 
 // Reference docs at https://spring.io/guides/gs/securing-web
 @Configuration
