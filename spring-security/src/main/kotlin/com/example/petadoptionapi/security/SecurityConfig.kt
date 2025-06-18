20c0a1fa46d35b3030c571f699f8b47bfdee0ca2
mai-thao <mthaominn@gmail.com>
2025-06-17 23:52:21 -0500

    Create basic security configuration

---
+++ spring-security/src/main/kotlin/com/example/petadoptionapi/security/SecurityConfig.kt
@@ -0,0 +1,42 @@
+package com.example.petadoptionapi.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+// Reference docs at https://spring.io/guides/gs/securing-web
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize(HttpMethod.GET, "/pet/**", permitAll)
+                authorize(HttpMethod.POST, "/pet/**", authenticated)
+                authorize(HttpMethod.PUT, "/pet/**", authenticated)
+                authorize(HttpMethod.DELETE, "/pet/**", authenticated)
+                authorize(anyRequest, denyAll)
+            }
+            httpBasic { }
+        }
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user = User.withUsername("user")
+            .password("password")
+            .roles("USER")
+            .build()
+
+        return InMemoryUserDetailsManager(user)
+    }
+}
