9f2a1fb63e5508cf04696b1bda86477052588270
mai-thao <mthaominn@gmail.com>
2025-06-20 15:09:28 -0500

    Implement controller logic based on in-memory pets

--- spring-security/src/main/kotlin/com/example/petadoptionapi/controller/ControllerSimpleAuth.kt
+++ spring-security/src/main/kotlin/com/example/petadoptionapi/controller/ControllerSimpleAuth.kt
@@ -1,29 +1,57 @@
 package com.example.petadoptionapi.controller
 
-import com.example.petadoptionapi.model.AdoptionStatus
 import com.example.petadoptionapi.model.Pet
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/pets")
 class PetAdoptionController {
+    @GetMapping("/{id}")
+    fun getPet(@PathVariable id: Long): ResponseEntity<Pet> {
+        val existingPet = pets.firstOrNull { it.id == id }
+        if (existingPet != null) {
+            return ResponseEntity.ok(existingPet)
+        } else {
+            return ResponseEntity.notFound().build()
+        }
+    }
+
     @GetMapping
-    fun getAll(): List<Pet> = TODO()
+    fun getAll(): ResponseEntity<List<Pet>> = ResponseEntity.ok(pets)
 
     @PostMapping
-    fun createPet(@RequestBody petInfo: Pet): Pet = TODO()
+    fun createPet(@RequestBody newPet: Pet): ResponseEntity<Pet> {
+        pets.add(newPet)
+        return ResponseEntity.status(HttpStatus.CREATED).body(newPet)
+    }
 
     @PutMapping("/{id}")
-    fun updatePet(@PathVariable id: Long, @RequestBody petInfo: Pet): Pet {
-        TODO()
+    fun updatePet(@PathVariable id: Long, @RequestBody newPet: Pet): ResponseEntity<Pet> {
+        val existingPetIdx = pets.indexOfFirst { newPet.id == id }
+        if (existingPetIdx == -1) {
+            return ResponseEntity.notFound().build()
+        } else {
+            pets[existingPetIdx] = newPet
+            return ResponseEntity.ok(newPet)
+        }
     }
 
     @DeleteMapping("/{id}")
-    fun deletePet(@PathVariable id: Long): Nothing = TODO()
+    fun deletePet(@PathVariable id: Long): ResponseEntity<Nothing> {
+        val existingPetIdx = pets.removeIf { it.id == id }
+        if (existingPetIdx) {
+            return ResponseEntity.noContent().build()
+        } else {
+            return ResponseEntity.notFound().build()
+        }
+    }
 
+    // List of dummy existing pets
     private val pets = mutableListOf(
-        Pet(4827192342, "Lucy", 7, "Husky", AdoptionStatus.AVAILABLE),
-        Pet(9481729301, "Buddy", 2, "Husky", AdoptionStatus.AVAILABLE),
-        Pet(7182346192, "Lucky", 4, "Husky", AdoptionStatus.AVAILABLE)
+        Pet(123, "Lucy", 7, "Husky", "Female"),
+        Pet(456, "Buddy", 2, "German Shepherd", "Male"),
+        Pet(789, "Lucky", 4, "Husky", "Male")
     )
 }
