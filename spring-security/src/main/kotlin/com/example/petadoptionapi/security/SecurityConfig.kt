cbc4ab138727949195e1b7dab5776a47d22726e2
mai-thao <mthaominn@gmail.com>
2025-06-20 15:32:56 -0500

    Create ADMIN role and add class comment

--- spring-security/src/main/kotlin/com/example/petadoptionapi/security/SecurityConfig.kt
+++ spring-security/src/main/kotlin/com/example/petadoptionapi/security/SecurityConfig.kt
@@ -11,7 +11,16 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.config.annotation.web.invoke // Must explicitly import this "invoke" fxn to enable the Kotlin DSL
 
-// Reference docs at https://spring.io/guides/gs/securing-web and https://docs.spring.io/spring-security/reference/servlet/configuration/kotlin.html
+/**
+ * A super simple basic authentication configuration with hardcoded in-memory user credentials.
+ * Pair this security config with the "ControllerSimpleAuth" class.
+ *
+ * ALl GET requests to the `/pets` URI have no restrictions
+ * All POST, PUT requests to the `/pets` URI are accessible only if logged in (e.g. USER or ADMIN)
+ * ALL DELETE requests to the `/pets` URI are accessible only by an ADMIN user
+ *
+ * Reference doc: https://spring.io/guides/gs/securing-web and https://docs.spring.io/spring-security/reference/servlet/configuration/kotlin.html
+ */
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
@@ -20,11 +29,11 @@ class SecurityConfig {
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             authorizeHttpRequests {
-                authorize(HttpMethod.GET, "/pets/**", permitAll) // Allow all GET requests
-                authorize(HttpMethod.POST, "/pets/**", authenticated) // Force authentication for POST, PUT, DELETE requests
+                authorize(HttpMethod.GET, "/pets/**", permitAll)
+                authorize(HttpMethod.POST, "/pets/**", authenticated)
                 authorize(HttpMethod.PUT, "/pets/**", authenticated)
-                authorize(HttpMethod.DELETE, "/pets/**", authenticated)
-                authorize(anyRequest, denyAll) // Block all other requests
+                authorize(HttpMethod.DELETE, "/pets/**", hasRole("ADMIN"))
+                authorize(anyRequest, denyAll)
             }
             httpBasic { } // Enable basic HTTP authentication
             csrf { disable() } // Disable csrf since this is a stateless API, not a browser app that uses session & cookies
@@ -32,6 +41,7 @@ class SecurityConfig {
         return http.build()
     }
 
+    // Hardcoded, in-memory login credentials (MEANT FOR TESTING ONLY!)
     @Bean
     fun userDetailsService(): UserDetailsService {
         val user = User.withUsername("user")
@@ -39,6 +49,11 @@ class SecurityConfig {
             .roles("USER")
             .build()
 
-        return InMemoryUserDetailsManager(user)
+        val admin = User.withUsername("admin")
+            .password("{noop}adminpassword")
+            .roles("ADMIN")
+            .build()
+
+        return InMemoryUserDetailsManager(user, admin)
     }
 }
