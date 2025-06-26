92ccc5796d1d75e12aae536a7358c46fc1e2d7e3
mai-thao <mthaominn@gmail.com>
2025-06-25 21:35:33 -0500

    Add unauthorized controller tests

---
+++ spring-security/src/test/kotlin/com/example/petadoptionapi/SimpleAuthControllerTest.kt
@@ -0,0 +1,57 @@
+package com.example.petadoptionapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.*
+
+@SpringBootTest
+@AutoConfigureMockMvc // Both annotations load the full Spring context so we can test security and @PreAuthorize configurations
+class SimpleAuthControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `GET endpoints accessible without auth`() {
+        mockMvc.get("/pets/123")
+            .andExpect {
+                status { isOk() }
+            }
+
+        mockMvc.get("/pets")
+            .andExpect {
+                status { isOk() }
+            }
+    }
+
+    @Test
+    fun `POST endpoint return 401 without auth`() {
+        mockMvc.post("/pets")
+            .andExpect {
+                status { isUnauthorized() }
+            }
+    }
+
+    @Test
+    fun `PUT endpoint return 401 without auth`() {
+        mockMvc.put("/pets/123")
+            .andExpect {
+                status { isUnauthorized() }
+            }
+    }
+
+    @Test
+    fun `DELETE endpoints return 401 without auth`() {
+        mockMvc.delete("/pets/123")
+            .andExpect {
+                status { isUnauthorized() }
+            }
+
+        mockMvc.delete("/pets")
+            .andExpect {
+                status { isUnauthorized() }
+            }
+    }
+}
