4da74ed2e1fbb1d9b47c591f7b6f65850470899f
mai-thao <mthaominn@gmail.com>
2025-06-25 22:17:12 -0500

    Short note on why we use Spring Security Test library

--- spring-security/build.gradle.kts
+++ spring-security/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
 
-    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.security:spring-security-test") // Allows us to test Spring Security configurations with mocked users
 }
